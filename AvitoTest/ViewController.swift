fde26adbcf27b1119d0a7f259b0e9e14667ab12e
shutrup <magagaraev95@mail.ru>
2023-08-24 14:40:03 +0300

    Initial Commit

---
+++ AvitoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoTest
+//
+//  Created by Шарап Бамматов on 24.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
